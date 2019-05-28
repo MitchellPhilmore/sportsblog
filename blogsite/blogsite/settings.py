5f9d6f4884afb2c88fd8665b4e5cae888ca6e6d8
unknown <mitchellphilmore@gmail.com>
2019-05-28 01:07:18 -0400

    setup deployment to heroku

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*4-g90@1z&rf=lq0dqkb^eu$76@27z9q0$x=*zmct0lbv^9$&t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['msc-sports-blog.herokuapp.com']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'blogsite.urls'
@@ -118,7 +119,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'blog/media')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT,'static')
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
