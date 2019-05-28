28b7fc03bf22b9f9c403e6454f30cacf6ca6f59c
unknown <mitchellphilmore@gmail.com>
2019-05-28 19:24:33 -0400

    heroku

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT,'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
